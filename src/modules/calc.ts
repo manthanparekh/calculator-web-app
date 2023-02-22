bfab173d6bb89384605cfbad04212c1bf96d66ca
manthanparekh <manthan.parekh@outlook.com>
2023-02-21 21:48:51 -0500

    operation add

--- src/modules/calc.ts
+++ src/modules/calc.ts
@@ -1,22 +1,22 @@
-import App from '../App';
+import { Operation } from './calc';
+
+
 export enum InputType {
     Numerical, 
     Operator,
 }
 
 export enum OperationType {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Equals,
+    Add = 'add',
+    Subtract = 'subtract',
+    Multiply = 'multiply',
+    Divide = 'divide',
+    Equals = 'equals',
 }
 
 export type CalcInput =
     | { type: InputType.Numerical; value: number }
-    | {
-        operator: any; type: InputType.Operator; operation: OperationType 
-};
+    | { operator: any; type: InputType.Operator; operation: OperationType };
 
 export type CalcState = {
     displayValue: number;
@@ -32,37 +32,36 @@ type OperationsBuilder = {
     working: Operation;
 }
 
-const getOperation = (inputs: Array<CalcInput>): Array<Operation> => {
-    const builder = inputs.reduce<OperationBuilder>(
+const getOperations = (inputs: Array<CalcInput>): Array<Operation> => {
+    const builder: OperationsBuilder = inputs.reduce<OperationsBuilder>(
         (builder, input) => {
-        switch (input.type) {
-            case InputType.Numerical:
-                const prevValue = builder.working?.value || 0;
-                const newValue = prevValue * 10 + input.value;
-                return { ...builder, working: { value: newValue } };
+            switch (input.type) {
+                case InputType.Numerical:
+                    const prevValue = builder.working?.value || 0;
+                    const newValue = prevValue * 10 + input.value;
+                    return { ...builder, working: { ...builder.working, value: newValue } };
             
-            case InputType.Operator:
-                return {
-                    operations: [...builder.operations, builder.working],
-                    working: {operator: input.operator, value: 0},
-                };
-        }
-        
-        
+                case InputType.Operator:
+                    return {
+                        operations: [...builder.operations, builder.working],
+                        working: { operator: input.operator, value: 0 },
+                    };
+            }
         },
         {
             operations: [],
             working: { operator: OperationType.Add, value: 0 },
-        } as OperationsBuilder
+        } 
     );
     return builder.operations;
-}
+};
 
 const getState = (inputs: Array<CalcInput>): CalcState => {
     return { displayValue: 0 };
 };
 
 const Calc = {
+    getOperations,
     getState,
 }
 
--- src/modules/clac.test.ts
+++ src/modules/clac.test.ts
@@ -1,4 +1,25 @@
 import Calc, { CalcInput, InputType, OperationType } from './calc';
+import { Operation } from './calc';
+
+test('generates operations', () => {
+    const inputs: Array<CalcInput> = [
+        { type: InputType.Numerical, value: 1 },
+        { type: InputType.Numerical, value: 0 },
+        { type: InputType.Operator, operation: OperationType.Add },
+        { type: InputType.Numerical, value: 5 },
+        { type: InputType.Operator, operation: OperationType.Equals },
+    ];
+
+    const operations: Array<Operation> = [
+        { operator: OperationType.Add, value: 15 },
+        { operator: OperationType.Add, value: 5 },
+        { operator: OperationType.Equals, value: 0 },
+    ]
+
+    ;
+    expect(Calc.getOperations(inputs)).toEqual(operations);
+});
+
 
 test('derive state', () => {
     const inputs: Array<CalcInput> = [
